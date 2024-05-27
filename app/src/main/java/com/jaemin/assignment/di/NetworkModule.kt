c9cd6a40a0a0f556b780ac3742560c643381a866
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 20:58:22 +0900

    feat: NetworkModule 구현
    - API 통신 설정

---
+++ app/src/main/java/com/jaemin/assignment/di/NetworkModule.kt
@@ -0,0 +1,70 @@
+package com.jaemin.assignment.di
+
+import com.jaemin.assignment.BuildConfig
+import com.jaemin.assignment.api.UnsplashService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient().newBuilder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): Converter.Factory {
+        val json = Json { ignoreUnknownKeys = true }
+        return json.asConverterFactory("application/json".toMediaType())
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.UNSPLASH_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUnsplashService(retrofit: Retrofit): UnsplashService =
+        retrofit.create(UnsplashService::class.java)
+}
