7878dd42b82441b0150f46993a9b74a26f6d205e
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 13:52:24 +0900

    feat: Bottom Navigation(Bar) 구현

---
+++ app/src/main/java/com/jaemin/assignment/navigation/Navigation.kt
@@ -0,0 +1,54 @@
+package com.jaemin.assignment.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+sealed class BottomNavItem(val route: String, val title: String, val icon: ImageVector) {
+    data object Feed : BottomNavItem("feed", "Feed", Icons.Default.Search)
+    data object Favorites : BottomNavItem("favorites", "Favorites", Icons.Default.Favorite)
+}
+
+@Composable
+fun AssignmentBottomBar(
+    destinations: List<BottomNavItem>,
+    navController: NavHostController
+) {
+    NavigationBar {
+        val currentRoute = currentRoute(navController = navController)
+        destinations.forEach { destination ->
+            NavigationBarItem(
+                selected = currentRoute == destination.route,
+                onClick = {
+                    navController.navigate(destination.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Icon(
+                        imageVector = destination.icon,
+                        contentDescription = destination.title
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
+}
