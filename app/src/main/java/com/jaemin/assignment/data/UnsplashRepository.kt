701e7dc432bc1465e7be764763d6c5a67b509d12
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 21:08:49 +0900

    feat: 페이징 데이터 스트림 가져오는 메서드 구현

--- app/src/main/java/com/jaemin/assignment/data/UnsplashRepository.kt
+++ app/src/main/java/com/jaemin/assignment/data/UnsplashRepository.kt
@@ -4,6 +4,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.jaemin.assignment.api.UnsplashService
+import com.jaemin.assignment.data.model.UnsplashPhoto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -11,7 +16,8 @@ import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(
-    private val dataStore: DataStore<Preferences>
+    private val dataStore: DataStore<Preferences>,
+    private val unsplashService: UnsplashService
 ) {
     private val favoritesKey = stringSetPreferencesKey("favorites")
 
@@ -31,5 +37,16 @@ class UnsplashRepository @Inject constructor(
             val currentFavorites = preferences[favoritesKey] ?: emptySet()
             preferences[favoritesKey] = currentFavorites - photoId
         }
+    }
+
+    fun getUnsplashPhotosStream(query: String): Flow<PagingData<UnsplashPhoto>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = PAGING_SIZE),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashService, query) }
+        ).flow
+    }
+
+    companion object {
+        private const val PAGING_SIZE = 30
     }
 }
