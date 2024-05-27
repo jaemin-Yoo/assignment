a98368db7e85b16a3e9fde3c147654e1e09bb354
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 20:56:48 +0900

    feat: 이미지 검색 API 구현

---
+++ app/src/main/java/com/jaemin/assignment/api/UnsplashService.kt
@@ -0,0 +1,17 @@
+package com.jaemin.assignment.api
+
+import com.jaemin.assignment.BuildConfig
+import com.jaemin.assignment.data.model.UnsplashSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UnsplashService {
+
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int,
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_ACCESS_KEY
+    ): UnsplashSearchResponse
+}
