616b2ef5a94f645a5d42017da45374bffe810339
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 21:07:17 +0900

    feat: PagingSource 정의

---
+++ app/src/main/java/com/jaemin/assignment/data/UnsplashPagingSource.kt
@@ -0,0 +1,35 @@
+package com.jaemin.assignment.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.jaemin.assignment.api.UnsplashService
+import com.jaemin.assignment.data.model.UnsplashPhoto
+
+private const val UNSPLASH_STARTING_PAGE_INDEX = 1
+
+class UnsplashPagingSource(
+    private val unsplashService: UnsplashService,
+    private val query: String
+) : PagingSource<Int, UnsplashPhoto>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UnsplashPhoto> {
+        val page = params.key ?: UNSPLASH_STARTING_PAGE_INDEX
+        return try {
+            val response = unsplashService.searchPhotos(query, page, params.loadSize)
+            val photos = response.results
+            LoadResult.Page(
+                data = photos,
+                prevKey = if (page == UNSPLASH_STARTING_PAGE_INDEX) null else page - 1,
+                nextKey = if (page == response.totalPages) null else page + 1
+            )
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, UnsplashPhoto>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey
+        }
+    }
+}
