77d82d9ab7cd206a517e10fea8400a1b1bf84c1a
jaemin-Yoo <iamjm29@gmail.com>
2024-05-27 20:51:16 +0900

    chore: Network 의존성 추가
    - retrofit2, okhttp3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,14 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.49")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     kapt("com.google.dagger:hilt-compiler:2.49")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
     implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
 }
