c998e02866af1772e1aceaa309f04863e73aa12f
jaemin-Yoo <iamjm29@gmail.com>
2024-05-29 00:36:23 +0900

    feat: PhotoList, Item Preview 구현

--- app/src/main/java/com/jaemin/assignment/ui/photo/UnsplashPhotoListScreen.kt
+++ app/src/main/java/com/jaemin/assignment/ui/photo/UnsplashPhotoListScreen.kt
@@ -30,15 +30,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.jaemin.assignment.R
+import com.jaemin.assignment.factory.UnsplashPhotoFactory
 import com.jaemin.assignment.model.UnsplashPhoto
-import com.jaemin.assignment.model.UnsplashPhotoUrls
 import com.jaemin.assignment.ui.theme.AssignmentTheme
 
 @Composable
@@ -89,8 +87,8 @@ fun UnsplashPhotoListScreen(
         ) { index ->
             val photo = unsplashPhotos[index] ?: return@items
             UnsplashPhotoListItem(
-                isFavoritePhoto = favoritePhotos.any { it.id == photo.id },
                 unsplashPhoto = photo,
+                isFavoritePhoto = favoritePhotos.any { it.id == photo.id },
                 onClickLikeButton = onClickLikeButton
             )
         }
@@ -100,8 +98,8 @@ fun UnsplashPhotoListScreen(
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun UnsplashPhotoListItem(
-    isFavoritePhoto: Boolean,
     unsplashPhoto: UnsplashPhoto,
+    isFavoritePhoto: Boolean = false,
     onClickLikeButton: (UnsplashPhoto, Boolean) -> Unit = { _, _ -> }
 ) {
     Card(
@@ -154,31 +152,18 @@ fun UnsplashPhotoListItem(
 
 @Preview
 @Composable
-private fun UnsplashPhotoListScreenPreview(
-    @PreviewParameter(UnsplashPhotoListPreviewParamProvider::class) unsplashPhotos: List<UnsplashPhoto>
-) {
+private fun UnsplashPhotoListItemPreview() {
     AssignmentTheme {
-        UnsplashPhotoListScreen(unsplashPhotos = unsplashPhotos)
+        val unsplashPhoto = UnsplashPhotoFactory.createUnsplashPhoto()
+        UnsplashPhotoListItem(unsplashPhoto =  unsplashPhoto)
     }
 }
 
-private class UnsplashPhotoListPreviewParamProvider :
-    PreviewParameterProvider<List<UnsplashPhoto>> {
-    override val values: Sequence<List<UnsplashPhoto>> =
-        sequenceOf(
-            listOf(
-                UnsplashPhoto(
-                    "1",
-                    UnsplashPhotoUrls("https://images.unsplash.com/photo-1715954582482-82f25cc96e78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTYzNjV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTY3OTE2OTR8&ixlib=rb-4.0.3&q=80&w=400")
-                ),
-                UnsplashPhoto(
-                    "2",
-                    UnsplashPhotoUrls("https://images.unsplash.com/photo-1715954582482-82f25cc96e78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTYzNjV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTY3OTE2OTR8&ixlib=rb-4.0.3&q=80&w=400")
-                ),
-                UnsplashPhoto(
-                    "3",
-                    UnsplashPhotoUrls("https://images.unsplash.com/photo-1715954582482-82f25cc96e78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTYzNjV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTY3OTE2OTR8&ixlib=rb-4.0.3&q=80&w=400")
-                )
-            )
-        )
+@Preview
+@Composable
+private fun UnsplashPhotoListScreenPreview() {
+    AssignmentTheme {
+        val unsplashPhotos = UnsplashPhotoFactory.createUnsplashPhotos(5)
+        UnsplashPhotoListScreen(unsplashPhotos = unsplashPhotos)
+    }
 }
