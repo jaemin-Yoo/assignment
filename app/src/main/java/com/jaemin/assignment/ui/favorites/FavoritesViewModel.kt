9318d3ea1c58d2e82874a96a53b45ad1483e619d
jaemin-Yoo <iamjm29@gmail.com>
2024-05-28 19:08:06 +0900

    feat: 즐겨찾기 화면 UiState 적용

--- app/src/main/java/com/jaemin/assignment/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/jaemin/assignment/ui/favorites/FavoritesViewModel.kt
@@ -2,11 +2,15 @@ package com.jaemin.assignment.ui.favorites
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import com.jaemin.assignment.data.UnsplashRepository
 import com.jaemin.assignment.model.UnsplashPhoto
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,8 +20,24 @@ class FavoritesViewModel @Inject constructor(
     private val unsplashRepository: UnsplashRepository
 ) : ViewModel() {
 
-    val favoritePhotos: StateFlow<Collection<UnsplashPhoto>> = unsplashRepository.favoritePhoto
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+    private val _favoritesUiState: MutableStateFlow<FavoritesUiState> =
+        MutableStateFlow(FavoritesUiState.Empty)
+    val favoritesUiState = _favoritesUiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            unsplashRepository.favoritePhoto
+                .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+                .collectLatest { collection ->
+                    val favoritesPhotos = collection.toList()
+                    if (favoritesPhotos.isNotEmpty()) {
+                        _favoritesUiState.value = FavoritesUiState.Success(favoritesPhotos)
+                    } else {
+                        _favoritesUiState.value = FavoritesUiState.Empty
+                    }
+                }
+        }
+    }
 
     fun addFavorite(photo: UnsplashPhoto) {
         viewModelScope.launch {
@@ -29,5 +49,17 @@ class FavoritesViewModel @Inject constructor(
         viewModelScope.launch {
             unsplashRepository.removeFavorite(photo.id, photo.urls.small)
         }
+    }
+
+    sealed interface FavoritesUiState {
+        data object Empty : FavoritesUiState
+
+        data object Loading : FavoritesUiState
+
+        data object Error : FavoritesUiState
+
+        data class Success(
+            val favoritePhotos: List<UnsplashPhoto>
+        ) : FavoritesUiState
     }
 }
