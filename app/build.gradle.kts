93f2ed2dad5b6768d2558931a4ca429b0d2e1eda
jaemin-Yoo <iamjm29@gmail.com>
2024-05-29 02:17:27 +0900

    chore: mockk, truth 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,9 +78,14 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.paging:paging-common-android:3.3.0")
     implementation("androidx.paging:paging-compose-android:3.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
